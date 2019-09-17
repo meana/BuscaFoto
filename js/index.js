56b7183836cbeb65f7cb2055a90ed0a67b2ee1ac
rmeanavallin <rmeanavallin@dxc.com>
2019-09-17 11:51:44 +0200

    new features added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
+let score = 0;
+
 $( document ).ready(function() {
 
     let selectedPhotoIndex = 0;
-    let score = 0;
 
     $('#photo').attr('src', photos[selectedPhotoIndex].imgRef);
     $('#photoTitle').text(photos[selectedPhotoIndex].name);
@@ -17,8 +18,9 @@ $( document ).ready(function() {
         //todo Join in the same function due to syc issues.
         showMonumentLocation(selectedPhotoIndex);
         generateScoringZone(selectedPhotoIndex);
-        console.log(markersArray.length);
+        createLine(markersArray[0].position, markersArray[1].position)
         score += calculateScore(markersArray[0], markersArray[1]);
+        showSummary(markersArray[0], markersArray[1]);
         $('#confirm').prop( "disabled", true );
         $('#cancel').prop( "disabled", true );
         $('#next').prop( "disabled", false );
@@ -36,9 +38,10 @@ $( document ).ready(function() {
         initializeButtons();
         removeMarker();
         removeScoringZone();
+        removeLine();
         enableMarkerCreation();
+        initializeSummary();
         if(selectedPhotoIndex === photos.length-1){
-            alert ("Your score is: " + score);
             initializeButtons();
         } else {
             selectedPhotoIndex+=1;
@@ -86,18 +89,24 @@ function showMonumentLocation(selectedPhotoIndex){
 
 function generateScoringZone(selectedPhotoIndex){
     let monument = new google.maps.LatLng(photos[selectedPhotoIndex].lat, photos[selectedPhotoIndex].lng);
-    let radio = 25000;
-    for(radio; radio <= 200000; radio+=50000){
+    let radio = 200000;
+    let colorIndex = 0;
+    let opacity = 0.15;
+    let colors = ['#FF0000','#FF7F00','#FFFF00','#00FF00'];
+
+    for(radio; radio >= 25000; radio-=50000){
         var circleOptions = {
             strokeColor: '#0000FF',
             strokeOpacity: 0.8,
             strokeWeight: 2,
-            fillColor: '#00FF00',
-            fillOpacity: 0.15,
+            fillColor: colors[colorIndex],
+            fillOpacity: opacity,
             map: map,
             center: monument,
             radius: radio
         };
+        colorIndex++;
+        opacity+=0.05;
         circleArray.push(new google.maps.Circle(circleOptions));
     }
 }
@@ -125,5 +134,36 @@ function calculateScore(marker, monument){
     } else {
         return 0;
     }
+}
+
+function createLine(marker, monument) {
+    var polyOptions = {
+        strokeColor: '#000000',
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+    }
+
+    poly = new google.maps.Polyline(polyOptions);
+    poly.setMap(map);
+
+    let path = poly.getPath();
+    path.push(marker);
+    path.push(monument);
+}
+
+function removeLine(){
+    poly.setMap(null);
+}
+
+function initializeSummary(){
+    $('#distance').text("Distancia: ");
+    $('#score').text("Puntuación obtenida: ");
+}
+
+function showSummary(marker, monument){
+    let distance = google.maps.geometry.spherical.computeDistanceBetween(marker.position, monument.position);
+    $('#distance').text("Distancia: " + Math.round(distance)/1000 + " Km");
+    $('#score').text("Puntuación obtenida: " + calculateScore(marker, monument));
+    $('#globalScore').text("Puntuación Global: " + score);
 }
 
--- js/mapManager.js
+++ js/mapManager.js
@@ -2,49 +2,21 @@ var map;
 var spain_geographical_center_lat = 40.308611;
 var spain_geographical_center_lng = -3.684444;
 
-var myMap =
-    [
-        {
-            "elementType": "labels",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "administrative.land_parcel",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "administrative.neighborhood",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        }
-    ];
-
 var markersArray = [];
 var circleArray = [];
+var poly;
 
 function initMap(listener) {
     var spain_geographical_center = new google.maps.LatLng(spain_geographical_center_lat, spain_geographical_center_lng);
     var mapOptions = {
         center: spain_geographical_center,
         zoom: 6,
-        mapTypeId: 'myMap',
+        mapTypeId: 'satellite',
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false
     };
     map = new google.maps.Map(document.getElementById("map_frame"), mapOptions);
-    map.mapTypes.set('myMap', new google.maps.StyledMapType(myMap, { name: 'My Map' }));
 
     var createMarker = map.addListener('click', function(e) {
         var marker = new google.maps.Marker({
